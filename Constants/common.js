7370b1ff61acac9e5cd5a3335c3c12011349b3de
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-07 15:38:39 +0900

    Changed exo name

--- Constants/common.js
+++
@@ -1,6 +0,0 @@
-const common = {
-  CURRENT_LOCATION: 'Current Location',
-  WHERE_TO: 'Where To'
-};
-
-export default common;
--- credentials.js
+++
@@ -1,5 +0,0 @@
-export default credentials = {
-  GOOGLE : {
-    APIKEY : 'AIzaSyDfOneTt2r36sWP_YLszbVLg9DnkbJENS0'
-  }
-}
