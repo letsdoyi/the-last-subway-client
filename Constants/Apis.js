a39bbc40b50e293e81782d7225ccee465b72b39d
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-08 19:34:42 +0900

    Modified constants to Upperclass

--- Constants/Apis.js
+++ Constants/Apis.js
@@ -1,12 +1,12 @@
-export { googleAPI };
-const googleAPI = {
-  directions: {
+export { GOOGLE_API };
+const GOOGLE_API = {
+  DIRECTIONS: {
     URL: 'https://maps.googleapis.com/maps/api/directions/json',
     MODE: 'transit',
     TRANSIT_MODE: 'subway',
     LANGUAGE: 'ko',
   },
-  geocode: {
+  GEOCODE: {
     URL: 'https://maps.googleapis.com/maps/api/geocode/json',
     LANGUAGE: 'ko',
   },
