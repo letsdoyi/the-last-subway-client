2d93514949bb56aac9de6fbd605d6efc8dbbc62c
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-08 19:33:01 +0900

    Modified SetAlarm styles

--- Components/SetAlarm.js
+++ Components/SetAlarm.js
@@ -1,70 +1,69 @@
 import React from 'react';
 import MyMapContainer from './MyMapContainer';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import MultipleSelection from './MultipleSelection';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import { relativeTimeRounding } from 'moment';
 
 export default function SetAlarm(props) {
   // console.log('SetAlarm props:', props);
-  const { container, header, title, front } = styles;
+  const { container, header, title, containerWrapper } = styles;
   return (
-    <View style={container}>
-      <View style={header}>
+    <View style={containerWrapper}>
+      <View style={[header]}>
         <Text>Cancel</Text>
         <Text style={title}>Set Alarm</Text>
         <Text onPress={() => {}}>Save</Text>
       </View>
-      <View style={styles.container}>
-        {/* <TextInput placeholder="Current Location"></TextInput> */}
-      </View>
-      <View style={styles.container}>
-        {/* <TextInput placeholder="Where to?" /> */}
-        <MyMapContainer
-          style={styles.container}
-          screenProps={props.screenProps}
-        />
-      </View>
-      <View style={styles.container}>
-        <Text style={styles.title}>AHEAD OF TIME ACTIVE</Text>
-        <ScrollView horizontal={true} style={styles.list}>
-          <Button title="5 mins"></Button>
-          <Button title="10 mins"></Button>
-          <Button title="15 mins"></Button>
-          <Button title="1 hour"></Button>
-          <Button title="1.5 hours"></Button>
-          <Button title="2 hours"></Button>
-        </ScrollView>
+
+      <MyMapContainer style={{}} screenProps={props.screenProps} />
+
+      <View style={styles.buttonTomultipleSelection}>
+        <Button
+          style={{}}
+          title="DONE"
+          onPress={() => {
+            props.navigation.navigate('MultipleSelection');
+          }}></Button>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  containerWrapper: {
+    position: 'relative',
+    flexDirection: 'column',
+  },
   header: {
-    // height: 20,
+    position: 'absolute',
+    top: 0,
+    minHeight: 20,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
   title: {
-    fontWeight: '700',
+    width: '45%',
+    lineHeight: 80,
+    paddingLeft: 10,
   },
-  container: {
-    minHeight: 20,
-    flexDirection: 'column',
-    // backgroundColor: '#ddd'
+  myMapContainer: {
+    position: 'absolute',
+    top: 0,
   },
-  list: {
-    // flexDirection: 'row',
-    // justifyContent: 'space-evenly',
-  },
-  flexOne: {
-    flex: 1,
-  },
-
-  flexTwo: {
-    flex: 2,
-  },
-
-  front: {
-    zIndex: 100,
+  buttonTomultipleSelection: {
+    position: 'absolute',
+    left: '50%',
+    bottom: '20%',
+    transform: [{ translateX: -Dimensions.get('window').width * 0.45 }],
+    width: '90%',
+    backgroundColor: '#000',
   },
 });
