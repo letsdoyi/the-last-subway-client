1cab795438c0fdb75d6b4a0a631e03041b9c5901
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-09 12:12:12 +0900

    Fixed no changing state bug

--- Reducers/alarmTimersReducer.js
+++ Reducers/alarmTimersReducer.js
@@ -4,8 +4,11 @@ const { SAVED_ALARM_TIMERS } = actionTypes;
 function alarmTimersReducer(state = [], action) {
   switch (action.type) {
     case SAVED_ALARM_TIMERS:
-      return [...state];
+      console.log('in alarmTimersReducer:',action.data);
+      return [...action.data];
+
     default:
+      console.log('in alarmTimersReducer default')
       return state;
   }
 }
