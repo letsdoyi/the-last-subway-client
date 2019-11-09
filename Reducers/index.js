9c995f239e06cea939f9daf74fa8b397882bab77
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-09 13:22:53 +0900

    Added dispatchers on directions

--- Reducers/index.js
+++ Reducers/index.js
@@ -2,9 +2,17 @@ import { combineReducers } from 'redux';
 import loginReducer from '../Reducers/loginReducer';
 import fromReducer from './fromReducer';
 import toReducer from './toReducer';
+import alarmSwitchReducer from './alarmSwitchReducer';
+import directionsReducer from './directionsReducer';
+import alarmTimersReducer from './alarmTimersReducer';
+import directionsReadySwitchReducer from './directionsReadySwitchReducer';
 
 export default combineReducers({
   // isLoggedIn: loginReducer,
   from: fromReducer,
-  to: toReducer
+  to: toReducer,
+  alarmTimers: alarmTimersReducer,
+  isAlarmOn: alarmSwitchReducer,
+  directions: directionsReducer,
+  isReadyToGetDirections: directionsReadySwitchReducer
 });
