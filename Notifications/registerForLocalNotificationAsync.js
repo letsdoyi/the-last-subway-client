89cb4c3edad09119f44f0e3fbdbb84024bd8d7fc
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-09 15:31:48 +0900

    Added connecting notification channel

--- Notifications/registerForLocalNotificationAsync.js
+++ Notifications/registerForLocalNotificationAsync.js
@@ -1,12 +1,20 @@
 //for noticifation
 import * as Permissions from 'expo-permissions';
+import noticifationConstants from '../Constants/notification';
 import { Notifications } from 'expo';
+import { Platform } from 'react-native';
+import { max } from 'moment';
 
 export default async function registerForLocalNotificationsAsync() {
+  console.log('registerForLocalNotificationsAsync ON');
+  console.log('noticifationConstants:', noticifationConstants);
+  const { CHANNEL_ID, PRIORITY, TITLE, BODY, DATA, VIBRATE, SOUND_ON, ICON_URL, LINK_URL, TIME } = noticifationConstants;
+  const localNotificationIdsQueue = [];
+
   const { status: existingStatus } = await Permissions.getAsync(
     Permissions.NOTIFICATIONS
   );
-  let finalStatus = existingStatus;
+  const finalStatus = existingStatus;
 
   // only ask if permissions have not already been determined, because
   // iOS won't necessarily prompt the user a second time.
@@ -19,27 +27,42 @@ export default async function registerForLocalNotificationsAsync() {
 
   // Stop here if the user did not grant permissions
   if (finalStatus !== 'granted') {
-    // alert('use phone');
+    // alert('use phone!');
     return;
   }
-  console.log('registerForLocalNotificationsAsync ON');
 
-  const localNotification = {
-    title: 'title',
-    body: 'body',
-    data: 'data',
-    android: {
-      icon:
-        'https://cdn0.iconfinder.com/data/icons/seo-and-development-16/200/32-512.png',
-      link: 'https://www.google.co.kr',
-    },
-  };
-  const schedulingOptions = {
-    time: new Date().getTime() + 1000,
-  };
-  // Get the token that uniquely identifies this device
-  return await Notifications.scheduleLocalNotificationAsync(
-    localNotification,
-    schedulingOptions
-  );
+  if (Platform.OS === 'android') {
+    Notifications.createChannelAndroidAsync(CHANNEL_ID, {
+      name: CHANNEL_ID,
+      priority: PRIORITY.MAX,
+      vibrate: VIBRATE,
+    });
+  }
+
+  async function _createNotificationAsync() {
+    const notificationId = await Notifications.scheduleLocalNotificationAsync(
+      {
+        title: TITLE,
+        body: BODY,
+        data: DATA,
+        android: {
+          channelId: CHANNEL_ID,
+          sound: SOUND_ON,
+          icon: ICON_URL,
+          link: LINK_URL
+        },
+      },
+      {
+        time: TIME,
+      }
+    );
+
+    return notificationId;
+    // localNotificationIdsQueue.push(notificationId);
+    // localNotificationIdsQueue.forEach(id => {
+    //   Notifications.cancelScheduledNotificationAsync(id)
+    // });
+  }
+
+  return await _createNotificationAsync();
 }
