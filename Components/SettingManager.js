6252af174bf3d74e0ad4e7c2730f9206269d9694
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-11 00:33:55 +0900

    Added edit and delete function

--- Components/SettingManager.js
+++ Components/SettingManager.js
@@ -8,14 +8,34 @@ import {
   Switch,
   Dimensions,
 } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 export default function SettingManager(props) {
-  const { screenProps } = props;
+  // let { screenProps, navigation } = props;
+  // console.log('resetState:', screenProps.resetState);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.element}>DELETE</Text>
-      <Text style={styles.element}>EDIT</Text>
+      <TouchableHighlight
+        onPress={() => {
+          alert('Successfully Deleted');
+          props.screenProps.resetState();
+        }}
+        style={[styles.elementWrapper, { paddingRight: 50 }]}>
+        <Text style={styles.element}>DELETE</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        onPress={() => {
+          alert('Edit');
+          // console.log(props.nav);
+          // console.log(props);
+          // props.screenProps.editAlarmSetting();
+          props.navigation.navigate('SetAlarm', {});
+        }}
+        style={[styles.elementWrapper, { paddingLeft: 50 }]}>
+        <Text style={styles.element}>EDIT</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -25,11 +45,15 @@ const screenRatio = (width * height) / 100;
 const styles = StyleSheet.create({
   container: {
     height: height * 0.12,
-    backgroundColor:'#ddd',
+    backgroundColor: '#ddd',
     justifyContent: 'space-between',
-    alignItems:'flex-end',
+    alignItems: 'flex-end',
     flexDirection: 'row',
     paddingHorizontal: width * 0.05,
+  },
+  elementWrapper: {
+    paddingTop: 10,
+    paddingBottom: 0,
   },
   element: {
     fontSize: height / 50,
--- Reducers/alarmSwitchReducer.js
+++ Reducers/alarmSwitchReducer.js
@@ -1,7 +1,7 @@
 import common from '../Constants/common';
 import actionTypes from '../Constants/actionTypes';
 const { CURRENT_LOCATION } = common;
-const { SAVED_ALARM_SETTING, RESET_STATE } = actionTypes;
+const { SAVED_ALARM_SETTING, RESET_STATE, EDIT_ALARM_SETTING } = actionTypes;
 
 function alarmSwitchReducer(state = false, action) {
   console.log('alarmSwitchReducer ON');
@@ -11,6 +11,9 @@ function alarmSwitchReducer(state = false, action) {
 
     case RESET_STATE:
       return false;
+
+    case EDIT_ALARM_SETTING:
+      return false
 
     default:
       return state;
