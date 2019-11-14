c96da34fb6080026822c037579e1148510b11067
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-11-14 18:02:25 +0900

    Refreshed google Api

--- Constants/Apis.js
+++ Constants/Apis.js
@@ -2,9 +2,10 @@ export { GOOGLE_API };
 const GOOGLE_API = {
   DIRECTIONS: {
     URL: 'https://maps.googleapis.com/maps/api/directions/json',
-    MODE: 'transit',
-    TRANSIT_MODE: 'subway',
-    LANGUAGE: 'ko',
+    MODE: { TRANSIT: 'transit' },
+    TRANSIT_MODE: { SUBWAY: 'subway' },
+    LANGUAGE: { KO: 'ko', EN: 'en' },
+    TRANSIT_ROUTING_PREFERENCE: { FEWER_TRANSFERS: 'fewer_transfers', LESS_WALKING:'less_walking' },
   },
   GEOCODE: {
     URL: 'https://maps.googleapis.com/maps/api/geocode/json',
